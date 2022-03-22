36dad6e73dcf61bda2eae6e5a552f19936791b9d
Ivan Silva <ivankuartz@hotmail.com>
2022-03-22 14:02:24 -0300

    Wrote home page

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+const Home = () => (
+  <div className="homeDiv">
+    <h1>Welcome to our page!</h1>
+    <p className="textParagraph">
+      In the words of Sofia Kovalevskaya: many who have had an opportunity of
+      knowing any more about mathematics confuse it with arithmetic, and
+      consider it an arid science. In reality, however, it is a science which
+      requires a great amount of imagination.
+    </p>
+    <p className="textParagraph">
+      As said by Marston Morse: Mathematics are the result of mysterious powers which no one understands,
+      and which the unconscious recognition of beauty must play an important
+      part. Out of an infinity of designs a mathematician chooses one pattern
+      for beauty's sake and pulls it down to earth.
+    </p>
+  </div>
+);
+
+export default Home;
